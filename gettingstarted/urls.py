78739c66d8a9dcba605406a36b3fc30859b4be1f
Seth Bergman <seth.atxwebs@gmail.com>
2016-05-01 04:58:12 -0500

    added view templates

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -12,4 +12,15 @@ import hello.views
 urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
     url(r'^codebase', hello.views.codebase, name='codebase.html'),
+    url(r'^dependencies', hello.views.dependencies, name='dependencies.html'),
+    url(r'^config', hello.views.config, name='config.html'),
+    url(r'^backing-services', hello.views.backing, name='backing-services.html'),
+    url(r'^build-release-run', hello.views.build, name='build-release-run.html'),
+    url(r'^process', hello.views.process, name='process.html'),
+    url(r'^port-binding', hello.views.port, name='port-binding.html'),
+    url(r'^concurrency', hello.views.concurrency, name='concurrency.html'),
+    url(r'^disposability', hello.views.disposability, name='disposability.html'),
+    url(r'^dev-prod-parity', hello.views.devprod, name='dev-prod-parity.html'),
+    url(r'^logs', hello.views.logs, name='logs.html'),
+    url(r'^admin-process', hello.views.admin, name='admin-process.html'),
 ]
--- hello/views.py
+++ hello/views.py
@@ -8,7 +8,38 @@ def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
 
-
 def codebase(request):
-
     return render(request, 'codebase.html')
+
+def dependencies(request):
+    return render(request, 'dependencies.html')
+
+def config(request):
+    return render(request, 'config.html')
+
+def backing(request):
+    return render(request, 'backing-services.html')
+
+def build(request):
+    return render(request, 'build-release-run.html')
+
+def process(request):
+    return render(request, 'process.html')
+
+def port(request):
+    return render(request, 'port-binding.html')
+
+def concurrency(request):
+    return render(request, 'concurrency.html')
+
+def disposability(request):
+    return render(request, 'disposability.html')
+
+def devprod(request):
+    return render(request, 'dev-prod-parity.html')
+
+def logs(request):
+    return render(request, 'logs.html')
+
+def admin(request):
+    return render(request, 'admin-process.html')
