0b498cccf5ffc3770d6ad48e1a6fd4847b4d3c1f
Seth Bergman <seth.atxwebs@gmail.com>
2016-05-01 03:59:24 -0500

    added view templates

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -10,7 +10,51 @@ import hello.views
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
+    url(r'^$', hello.templates.index, name='index'),
     url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+configure do
+  I18n.enforce_available_locales = true
+  I18n.load_path = Dir[File.join(settings.root, 'locales', '*.yml')]
+  I18n.backend.load_translations
+  I18n.default_locale = :en
+end
+
+before do
+  I18n.locale = I18n.default_locale
+end
+
+before '/:locale/*' do
+  locale = params[:locale].to_sym
+  if locale != I18n.default_locale && I18n.available_locales.include?(locale)
+    I18n.locale = locale
+    request.path_info = '/' + params[:splat][0]
+  end
+end
+
+get '/' do
+  erb :home
+end
+
+TOC = %w(codebase dependencies config backing-services build-release-run processes port-binding concurrency disposability dev-prod-parity logs admin-processes)
+
+get '/:factor' do |factor|
+  halt 404 unless TOC.include?(factor)
+  @factor = factor
+  erb :factor
+end
+
+helpers do
+  def render_markdown(file)
+    markdown = File.read("content/#{I18n.locale}/#{file}.md", :encoding => 'utf-8')
+    Maruku.new(markdown).to_html
+  rescue Errno::ENOENT
+    puts "No content for #{I18n.locale}/#{file}, skipping"
+  end
+
+
+not_found do
+  "Page not found"
+end
